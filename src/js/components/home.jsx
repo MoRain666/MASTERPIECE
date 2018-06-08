9f62b89de5d7a15e4cc59b4a05aad93d94305449
V.Kharitonovitch <morainrallez@gmail.com>
2018-06-08 23:16:23 +0300

    add css-landing-page-file

--- src/js/components/home.jsx
+++ src/js/components/home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class Home extends React.Component{
     render(){
         return <div>
-        <h2>Главная</h2>
+        <h2 className='head'>Главная</h2>
         <h1>Тут будет LandingPage</h1>
         <a href="/game">game</a>
         </div>;
