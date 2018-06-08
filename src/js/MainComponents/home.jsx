29682fd6ad7145e7e195e991e56336912e981dd4
V.Kharitonovitch <morainrallez@gmail.com>
2018-06-09 01:24:37 +0300

    add files and folders for LandingPage

--- src/js/MainComponents/home.jsx
+++ src/js/MainComponents/home.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
+import Header from './ComponentsForHome/header';
 class Home extends React.Component{
     render(){
         return <div className='LandingPage'>
-        <h2>Главная</h2>
-        <h1>Тут будет LandingPage</h1>
-        <a href="/game">game</a>
+            <Header />
         </div>;
     }
 }
