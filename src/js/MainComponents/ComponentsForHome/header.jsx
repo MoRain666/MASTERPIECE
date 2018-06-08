6f9e010291364be5f13d00dcfd75dd726dfada74
V.Kharitonovitch <morainrallez@gmail.com>
2018-06-09 02:26:52 +0300

    added header to LandingPage

--- src/js/MainComponents/ComponentsForHome/header.jsx
+++ src/js/MainComponents/ComponentsForHome/header.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 class Header extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {linksName: ['Gameplay', 'Start to play', 'About developers', 'Who and what did'],
+                    linksRout: ['gamePlay', 'start', 'about', 'job']    
+    };
+    }
 
     render(){
-        return <header className='LandingPage-Header'>
-        </header>
+        const list = this.state.linksName.map((item, index)=>{
+            return <li key={index}><a href={`#${this.state.linksRout[index]}`}>{item}</a></li>;
+        });
+        console.log(list);
+        return <div className='LandingPage-Promo-and-Header'>
+            <header className='LandingPage-Header'>
+                <div className="LandingPage-Header-Logo"><a id='#top' href="#top">{'masterpiece'.toUpperCase()}</a></div>
+                <ul className='LandingPage-Header-Links-List'>
+                {list}
+                </ul>
+            </header>
+        </div>
     }
 }
 export default Header;
