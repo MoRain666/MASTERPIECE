3705ef838b1b2ca9e56d7110b5dd038cfc9b4ef4
V.Kharitonovitch <morainrallez@gmail.com>
2018-06-15 19:33:40 +0300

    init full PreGame section

--- src/js/MainComponents/PregameComponents/index.jsx
+++ src/js/MainComponents/PregameComponents/index.jsx
@@ -5,6 +5,7 @@ class PreGame extends React.Component{
         this.state = {
             aboutGame: 'Живи и пой, Анастасия, нас ждут великие дела!!!',
             isPlaying: true,
+            formRender: ''
         };
     }
 
@@ -23,16 +24,40 @@ class PreGame extends React.Component{
             audioButton.classList.remove('PreGame-PauseButton');
         }
     }
+
+    createNewUser(){
+
+    }
+
+    formRender(){
+        const form = <form className='PreGame-form'>
+            <h3>Here a form</h3>
+            <h4>First Name</h4>
+            <input type="text" required/>
+            <h4>Last Name</h4>
+            <input type="text" required/>
+            <input onClick={this.createNewUser} type="submit" value="START PLAY"/>
+        </form>;
+        setTimeout(()=>{
+            this.setState({formRender: form});
+            document.querySelector('#titles').remove();
+        }, 10000)
+    }
+    componentDidMount(){
+        this.formRender();
+    }
     
     render(){
-        return <div className='PreGame'>
-        <audio id="myAudio" src={require('../../../music/theme-song-superman.mp3')} autoPlay="true"></audio>
+        return <div id='PreGame' className='PreGame'>
+        <audio id="myAudio" src={require('../../../music/theme-song-superman.mp3')} autoPlay={this.state.isPlaying}></audio>
             <button className='audioButton PreGame-PlayButton' onClick={this.togglePlay.bind(this)}></button>
+            <img src={require('../../../img/PreGame/supermanFly.gif')} alt=""/>
             <div id="titles">
                 <div id="titlecontent">
 	                <p>{this.state.aboutGame}</p>
                 </div>
             </div>
+            {this.state.formRender}
         </div>
     }
 }
